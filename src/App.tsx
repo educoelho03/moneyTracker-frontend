447f03e2a9b393ce8f3150310aa1ab44f02a57bd
educoelho03 <eduardopaivacoelho@outlook.com>
2025-03-02 01:47:20 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { Routes, Route } from "react-router-dom";
-import DefaultLogin from "./app/components/DefaultLogin";
+import React from 'react';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import Login from './app/components/Login';
+import Signup from './app/components/Signup';
+import HomePage from './app/components/HomePage';
+
+// Componente de rota privada
+const PrivateRoute = () => {
+    const token = localStorage.getItem('jwtToken'); // Verifica se o token JWT está armazenado
+    return token ? <Outlet /> : <Navigate to="/login" />; // Se autenticado, renderiza o componente filho; se nao, redireciona para o login
+};
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<DefaultLogin isLoginPage={true} />} />
-            <Route path="/login" element={<DefaultLogin isLoginPage={true} />} />
-            <Route path="/signup" element={<DefaultLogin isLoginPage={false} />} />
+            {/* Rotas públicas */}
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+
+            {/* Rotas protegidas */}
+            <Route path="/home" element={<PrivateRoute />}>
+                <Route index element={<HomePage />} /> {/* Rota padrão para /home */}
+            </Route>
         </Routes>
     );
 }
--- src/app/components/PrimaryInput.tsx
+++
@@ -1,7 +0,0 @@
-export default function DefaultLogin() {
-    return (
-        <main>
-            
-        </main>
-    )
-}
